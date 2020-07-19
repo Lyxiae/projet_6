7bdfbd9f0c80c2ce842bbf9b1edbd55185b06eff
Lyxiae <robin.marjolaine@gmail.com>
2020-07-19 15:25:28 +0200

    Ajout des routes & logiques métier
    Ajout des routes sauces (sauf like), et des logiques métier pour les routes sauces (sans authentification ni gestion de fichiers pour l'instant).
    En prévision de discussion ou debug sur la route createSauce (problème de requête vide)
    les routes GET, PUT et DELETE fonctionnent (via postman pour PUT et DELETE)

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const path = require('path');
+// const path = require('path');
 
 const sauceRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
-
 
 const app = express();
 
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -3,18 +3,34 @@ const Sauce = require('../models/Sauce');
 //Importation du système de gestion de fichiers file system de Node
 const fs = require('fs');
 
+
 //Logique métier pour createSauce, créer une sauce
 exports.createSauce = (req, res, next) => {
     console.log(req.body);
-    // const sauceObject = JSON.parse(req.body);
-    // delete sauceObject._id;
-    // const sauce = new Sauce({
-    //     ...sauceObject,
-    // });
-    // console.log(sauce);
-    // sauce.save()
-    // .then(() => res.status(201).json({ message: 'Sauce créée !' }))
-    // .catch(error => res.status(400).json({ error }));
+    const sauceObject = req.body;
+    console.log(sauceObject);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject
+    });
+    console.log(sauce);
+    sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce créée !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+//Logique métier pour deleteSauce, supprime une sauce
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce Supprimée !' }))
+    .catch(error => res.status(500).json({ error }));
+}
+
+//Logique métier pour getAllSauces, obtient les informations de toutes les sauces
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
 };
 
 //Logique métier pour getOneSauce, obtient les informations d'une sauce d'après son id.
@@ -24,9 +40,12 @@ exports.getOneSauce = (req, res, next) => {
         .catch(error => res.status(404).json({ error }));
 };
 
-//Logique métier pour getAllSauces, obtient les informations de toutes les sauces
-exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-};
+//Logique métier pour modifySauce, modifie les informations d'une sauce
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.body;
+    console.log(req.body);
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+    .catch(error => res.status(400).json({ error }));
+}
+
